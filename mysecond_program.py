e1bd8b97f987a920622a30d36ce1dcce49f641a8
K.Chris Darwin Peries <134520499+Chrishdarwin100@users.noreply.github.com>
2023-06-14 19:06:35 +0530

    Add files via upload

---
+++ mysecond_program.py
@@ -0,0 +1,12 @@
+print("=================== Print My Age,Weight,Hight=====================")
+
+age=13
+print(age)
+
+weight = 56
+unit = "kg"
+print(str(weight) + unit)
+
+hight = 172
+unit = "cm"
+print(str(hight) + unit)
